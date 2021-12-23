Correcting problem in UF free energy and sign for liquid TI

mlacs/ti/liquids.py
@@ -127,7 +127,7 @@ class UFLiquidState(ThermoState):
         f_ig /= nat
 
         # Compute Uhlenbeck-Ford excess free energy
-        f_uf = free_energy_uhlenbeck_ford(1./vol/nat, self.p, self.sigma, self.temperature)
+        f_uf = free_energy_uhlenbeck_ford(nat/vol, self.p, self.sigma, self.temperature)
 
 
         # Compute the work between Uhlenbeck-Ford potential and the MLIP
@@ -137,9 +137,8 @@ class UFLiquidState(ThermoState):
         int_b = np.trapz(u1_b-u2_b, lambda_b)
 
         work  = (int_f - int_b) / 2.0
-        work /= nat
 
-        free_energy = f_uf  + work
+        free_energy = f_ig + f_uf  - work
         free_energy_corrected  = free_energy
         if self.fcorr1 is not None:
             free_energy_corrected += self.fcorr1

mlacs/utilities/thermo.py
@@ -103,28 +103,34 @@ def free_energy_uhlenbeck_ford(rho, p, sigma, T):
 
     if x < 0.0025:
         result = coef[0] * (x**2)/2.0 + coef[1] * x
-        return beta * result
+        return result/beta
+        #return beta * result
     elif x < 0.1:
         if x * 10000 % 25 == 0:
-            return beta * sum_spline[index-1]
+            #return beta * sum_spline[index-1]
+            return sum_spline[index-1] / beta
         else:
             x_0 = 0.0025 * int(x*400)
     elif x < 1:
         if x * 1000 % 25 == 0:
-            return beta * sum_spline[index-1]
+            #return beta * sum_spline[index-1]
+            return sum_spline[index-1] / beta
         else:
             x_0 = 0.025 * int(x*40)
     elif x < 4:
         if x * 100 % 10 == 0:
-            return beta * sum_spline[index-1]
+            #return beta * sum_spline[index-1]
+            return sum_spline[index-1] / beta
         else:
             x_0 = 0.1 * int(x*10)
     else:
-        return beta * sum_spline[index]
+        #return beta * sum_spline[index]
+        return sum_spline[index] / beta
 
     
     result =  sum_spline[index-1] + coef[0] * (x ** 2.0 - x_0 ** 2.0)/2.0 + coef[1] * (x - x_0) + (coef[2] - 1.0) * np.log(x/x_0) - coef[3] * (1.0/x - 1.0/x_0)
-    return beta * result
+    #return beta * result 
+    return result / beta
 
 
 