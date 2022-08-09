53649a7e0072fede460959adfaf3ddd0a37d1ae0
Aloïs Castellano <alois.castel@gmail.com>
2022-08-09 23:56:32 +0200

    I forgot to remove the interface with ASE md in the state.__init__

--- mlacs/state/__init__.py
+++ mlacs/state/__init__.py
@@ -3,15 +3,11 @@
 // This code is licensed under MIT license (see LICENSE.txt for details)
 """
 from mlacs.state.state import StateManager
-from mlacs.state.langevin import LangevinState
-from mlacs.state.verlet import VerletState
 from mlacs.state.lammps_state import LammpsState
 from mlacs.state.custom_lammps_state import CustomLammpsState
 from mlacs.state.ipi_state import IpiState
 
 __all__ = ['StateManager',
-           'LangevinState',
-           'VerletState',
            'LammpsState',
            'CustomLammpsState',
            'IpiState']
