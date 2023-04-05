41394423bc88897ef2f3ba8b3c057f2830f8acc5
pauli-nerd <p.richard797@laposte.net>
2023-04-05 15:32:52 +0200

    Mean and std Fe in .log for TI woth sevral instances

--- mlacs/ti/thermoint.py
+++ mlacs/ti/thermoint.py
@@ -13,20 +13,20 @@ from concurrent.futures import ThreadPoolExecutor
 # ========================================================================== #
 class ThermodynamicIntegration:
     """
-    Class to handle a series of thermodynamic integration
+    Class to handle a series of thermodynamic integration on sampled states
 
     Parameters
     ----------
     thermostate: :class:`thermostate` or :class:`list` of :class:`thermostate`
         State for which the thermodynamic integration should be performed
     ninstance: : class:`int` 
-        Numer of forward and backward to be performed
+        Numer of forward and backward to be performed, default 1
     logfile: :class:`str` (optional)
         Name of the logfile. Default ``\"ThermoInt.log\"``
     """
     def __init__(self,
                  thermostate,
-                 ninstance=10,
+                 ninstance=1,
                  logfile=None):
 
         self.log = ThermoLog(logfile)
@@ -52,7 +52,7 @@ class ThermodynamicIntegration:
             raise TypeError(msg)
         self.nstate = len(self.state)
         self.recap_state()
-        
+
 # ========================================================================== #
     def run(self):
         """
@@ -61,33 +61,71 @@ class ThermodynamicIntegration:
         with ThreadPoolExecutor(max_workers=self.ninstance) as executor:
             for istate in range(self.nstate):
                 executor.submit(self._run_one_state, istate)
-                msg = f"State {istate+1}/{self.nstate} launched"
-                stateworkdir = self.workdir + self.state[istate].get_workdir() + f"for_back_{istate+1}/"
+                msg = f"State {istate+1}/{self.nstate} launched\n"
+                stateworkdir = self.workdir + self.state[istate].get_workdir()
                 msg += f"Working directory for this state : \n{stateworkdir}\n"
                 self.log.logger_log.info(msg)
 
 # ========================================================================== #
     def _run_one_state(self, istate):
         """
+        Run the simulation for one state
         """
-        stateworkdir = self.workdir + self.state[istate].get_workdir() + f"for_back_{istate+1}/"
-        self.state[istate].run(stateworkdir)
-        msg = f"State {istate+1} : Molecular Dynamics Done\n"
-        msg += "Starting post-process\n"
-        self.log.logger_log.info(msg)
-        msg = '============================================================\n'
-        msg += f"State {istate+1} : Post-process Done\n"
-        msg += self.state[istate].postprocess(stateworkdir)
-        msg += '============================================================\n'
-        self.log.logger_log.info(msg)
+        if self.ninstance > 1:
+            for i in range(self.ninstance):
+                stateworkdir = self.workdir + self.state[istate].get_workdir() + f"for_back_{i+1}/"
+                self.state[istate].run(stateworkdir)
+                msg = f"State {istate+1} instance_{i+1} : Molecular Dynamics Done\n"
+                msg += "Starting post-process\n"
+                self.log.logger_log.info(msg)
+                msg = '============================================================\n'
+                msg += f"State {istate+1} instance_{i+1}: Post-process Done\n"
+                msg += self.state[istate].postprocess(stateworkdir)
+                msg += '============================================================\n'
+                self.log.logger_log.info(msg)
+            self.error(istate)
+        elif self.ninstance == 1: 
+            stateworkdir = self.workdir + self.state[istate].get_workdir()
+            self.state[istate].run(stateworkdir)
+            msg = f"State {istate+1}: Molecular Dynamics Done\n"
+            msg += "Starting post-process\n"
+            self.log.logger_log.info(msg)
+            msg = '============================================================\n'
+            msg += f"State {istate+1}: Post-process Done\n"
+            msg += self.state[istate].postprocess(stateworkdir)
+            msg += '============================================================\n'
+            self.log.logger_log.info(msg)
 
 # ========================================================================== #
     def recap_state(self):
         """
         """
-        msg = "Total number of state:{0}. One state is equivalent to one f/b\n".format(self.nstate)
+        msg = "Total number of state : {0}. One state is equivalent to ninstance f/b\n".format(self.nstate)
         for istate in range(self.nstate):
             msg += "State {0}/{1} :\n".format(istate+1, self.nstate)
             msg += self.state[istate].log_recap_state()
             msg += "\n\n"
         self.log.logger_log.info(msg)
+
+# ========================================================================== #
+    def error(self, istate):
+        """
+        Error and average in free energy instances for one state
+        Computed is ninstance > 1
+        """
+        import numpy as np
+        
+        stateworkdir = self.workdir + self.state[istate].get_workdir()
+        fe = []
+        for j in range(self.ninstance):
+            tmp_fe = np.loadtxt(stateworkdir + f"for_back_{j+1}/" + f"free_energy.dat")
+            fe.append(tmp_fe[1])
+        ferr = np.std(fe, axis = 0)
+        femean = np.mean(fe, axis = 0)
+        msg = f"Free Energy mean and error for state {istate+1}:\n"
+        msg += f"- Mean: {femean:10.6f}\n"
+        msg += f"- Error: {ferr:10.6f}\n"
+        self.log.logger_log.info(msg)
+
+        
+
