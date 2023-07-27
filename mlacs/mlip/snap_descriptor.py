6fabbbfb613a865a9e0b0ad2ae2f86df07c8739b
Aloïs Castellano <alois.castel@gmail.com>
2023-07-27 18:12:10 +0200

    correct bug for chemflag with snap descriptor

--- mlacs/mlip/snap_descriptor.py
+++ mlacs/mlip/snap_descriptor.py
@@ -52,7 +52,7 @@ class SnapDescriptor(Descriptor):
     """
     def __init__(self, atoms, rcut=5.0, parameters=dict(),
                  model="linear", alpha=1.0, alpha_quad=1.0, folder="Snap"):
-        self.chemflag = parameters.get("chemflag", 0)
+        self.chemflag = parameters.pop("chemflag", 0)
         Descriptor.__init__(self, atoms, rcut, alpha)
         self.alpha_quad = alpha_quad
         self.folder = Path(folder).absolute()
