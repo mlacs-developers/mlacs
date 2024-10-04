e2b2d3113ca03fd807b7863ee3a82dabb893a78c
SoutenanceDuval <clement.duval@lkb.upmc.fr>
2024-10-04 11:09:03 +0200

    Further merge-related adjustements

--- mlacs/otf_mlacs.py
+++ mlacs/otf_mlacs.py
@@ -1,5 +1,5 @@
 """
-// Copyright (C) 2022-2024 MLACS group (AC, RB, ON)
+// Copyright (C) 2022-2024 MLACS group (AC, RB, ON, CD)
 // This file is distributed under the terms of the
 // GNU General Public License, see LICENSE.md
 // or http://www.gnu.org/copyleft/gpl.txt .
