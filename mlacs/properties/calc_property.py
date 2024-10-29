Fix: change units in *HIST.nc
> The units saved in the *HIST.nc are now atomic units. These units were
previously taken from LAMMPS `metal` unit system, which led to incorrect
visualization when reading the *HIST.nc with Agate. Indeed, Agate expects
atomic units (Abinit preferred units).

> Note that the plot_thermo command is left unchanged, and still
returns results in {eV, Angstrom, GPa} units. Since this command reads
information from the *HIST.nc, now in atomic units, a separate conversion is
made directly in utilities.plots.py, from {Ha, Bohr} to {eV, Angstrom, GPa}.

mlacs/properties/calc_property.py
@@ -13,7 +13,7 @@ import numpy as np
 from operator import attrgetter
 
 from ase.atoms import Atoms
-from ase.units import GPa
+from ase.units import Hartree, Bohr
 
 from ..core.manager import Manager
 from ..utilities.io_lammps import LammpsBlockInput
@@ -443,6 +443,18 @@ class CalcExecFunction(CalcProperty):
 
     useatoms: :class:`bool`
         True if the function is called from an ase.Atoms object.
+
+    nc_unit: :class:`str` (optional)
+        Unit of the observable saved in *HIST.nc file.
+        These units are derived from the atomic unit system: Bohr, Ha, etc.
+        Cf. mlacs.utilities.io_abinit.HistFile._set_unit_conventions().
+        Default ''.
+
+    lammps_unit: :class:`str` (optional)
+        Unit of the observable as computed from LAMMPS.
+        Cf. mlacs.utilities.io_abinit.HistFile._set_unit_conventions().
+        These units are expected to be the `metal` units, cf. ase.units.py.
+        Default ''.
     """
 
     def __init__(self,
@@ -453,7 +465,8 @@ class CalcExecFunction(CalcProperty):
                  gradient=False,
                  criterion=0.001,
                  frequence=1,
-                 nc_unit=''):
+                 nc_unit='',
+                 lammps_unit=''):
         CalcProperty.__init__(self, args, None, 'max', criterion, frequence)
 
         self._func = function
@@ -467,6 +480,29 @@ class CalcExecFunction(CalcProperty):
         self.label = function
         self.shape = None
         self.nc_unit = nc_unit
+        self.lammps_unit = lammps_unit
+
+# ========================================================================== #
+    def _unit_converter(self):
+        """
+        Convert units from LAMMPS's `metal` convention to Abinit's, i.e.,
+        the result self.new of the _exec() routine is expressed in atomic unit
+        """
+        eV2Ha = 1/Hartree
+        Ang2Bohr = 1/Bohr
+        # Dictionary that maps Lammps units to the corresponding multiplication
+        # factors that convert them to Abinit units system.
+        # Example: if a = 100 eV then a*unit_convert_dict['eV'] is 3.67 Hartree
+        unit_convert_dict = {'eV': eV2Ha,
+                             'Ang': Ang2Bohr,
+                             'eV/Ang': eV2Ha/Ang2Bohr,
+                             'Ang^3': Ang2Bohr**3,
+                             'eV/Ang^3': eV2Ha/Ang2Bohr**3,
+                             }
+
+        # Proceed with the conversion itself
+        if hasattr(self, 'new') and self.lammps_unit in unit_convert_dict:
+            self.new *= unit_convert_dict[self.lammps_unit]
 
 # ========================================================================== #
     def _exec(self, wdir=None):
@@ -478,11 +514,7 @@ class CalcExecFunction(CalcProperty):
             self.new = np.r_[[_f(**self.kwargs) for _f in self._function]]
         else:
             self.new = self._function(**self.kwargs)
-        if self.nc_unit == 'GPa':
-            # Evaluation of _f(...) returns LAMMPS natural units, which are
-            # eV/Angs^3 for stresses. Here, conversion to GPa
-            self.new /= GPa
-
+        self._unit_converter()
         if self.isfirst:
             self.shape = self.new[0].shape
         return self.isconverged
@@ -524,12 +556,19 @@ class CalcRoutineFunction(CalcExecFunction):
 
     nc_dim: :class:`str` (optional)
         Name of the dimension of the observable in *HIST.nc file.
-        Cf. mlacs.utilities.io_abinit.HistFile.nc_routine_conv().
+        Cf. mlacs.utilities.io_abinit.HistFile._set_name_conventions().
         Default ``None``.
 
     nc_unit: :class:`str` (optional)
-        Name of the unit of the observable in *HIST.nc file.
-        Cf. mlacs.utilities.io_abinit.HistFile.nc_routine_conv().
+        Unit of the observable saved in *HIST.nc file.
+        These units are derived from the atomic unit system: Bohr, Ha, etc.
+        Cf. mlacs.utilities.io_abinit.HistFile._set_unit_conventions().
+        Default ''.
+
+    lammps_unit: :class:`str` (optional)
+        Unit of the observable as computed from LAMMPS.
+        Cf. mlacs.utilities.io_abinit.HistFile._set_unit_conventions().
+        These units are expected to be the `metal` units, cf. ase.units.py.
         Default ''.
 
     weight: :class:`WeightingPolicy` (optional)
@@ -542,18 +581,18 @@ class CalcRoutineFunction(CalcExecFunction):
                  nc_name=None,
                  nc_dim=None,
                  nc_unit='',
+                 lammps_unit='',
                  weight=None,
                  gradient=False,
                  criterion=None,
                  frequence=1):
-        CalcExecFunction.__init__(self, function, dict(), None, True,
-                                  gradient, criterion, frequence, nc_unit)
+        CalcExecFunction.__init__(self, function, dict(), None, True, gradient,
+                                  criterion, frequence, nc_unit, lammps_unit)
         self.weight = weight
         self.label = label
         self.needdir = False
         self.nc_name = nc_name
         self.nc_dim = nc_dim
-        self.nc_unit = nc_unit
 
 # ========================================================================== #
     def __repr__(self):
@@ -600,13 +639,15 @@ class CalcPressure(CalcRoutineFunction):
         label = 'Pressure'
         nc_name = 'press'
         nc_dim = ('time',)
-        nc_unit = 'GPa'
+        nc_unit = 'Ha/Bohr^3'
+        lammps_unit = 'eV/Ang^3'
         CalcRoutineFunction.__init__(self,
                                      'get_stress',
                                      label,
                                      nc_name,
                                      nc_dim,
-                                     nc_unit)
+                                     nc_unit,
+                                     lammps_unit)
 
     def _exec(self, wdir=None):
         """
@@ -614,10 +655,11 @@ class CalcPressure(CalcRoutineFunction):
         """
         if self.use_atoms:
             self._function = [getattr(_, self._func) for _ in self.atoms]
-            self.new = np.r_[[-np.mean(_f(**self.kwargs)[:3])/GPa
+            self.new = np.r_[[-np.mean(_f(**self.kwargs)[:3])
                               for _f in self._function]]
         else:
             self.new = self._function(**self.kwargs)
+        self._unit_converter()
         if self.isfirst:
             self.shape = self.new[0].shape
         return self.isconverged
@@ -643,13 +685,15 @@ class CalcAcell(CalcRoutineFunction):
         label = 'Acell'
         nc_name = 'acell'
         nc_dim = ('time', 'xyz')
-        nc_unit = 'Ang'
+        nc_unit = 'Bohr'
+        lammps_unit = 'Ang'
         CalcRoutineFunction.__init__(self,
                                      'get_cell_lengths_and_angles',
                                      label,
                                      nc_name,
                                      nc_dim,
-                                     nc_unit)
+                                     nc_unit,
+                                     lammps_unit)
 
     def _exec(self, wdir=None):
         """
@@ -689,12 +733,14 @@ class CalcAngles(CalcRoutineFunction):
         nc_name = 'angl'
         nc_dim = ('time', 'xyz')
         nc_unit = 'deg'
+        lammps_unit = 'deg'
         CalcRoutineFunction.__init__(self,
                                      'get_cell_lengths_and_angles',
                                      label,
                                      nc_name,
                                      nc_dim,
-                                     nc_unit)
+                                     nc_unit,
+                                     lammps_unit)
 
     def _exec(self, wdir=None):
         """
@@ -706,6 +752,7 @@ class CalcAngles(CalcRoutineFunction):
             self.new = np.r_[[_f(**self.kwargs)[3:] for _f in self._function]]
         else:
             self.new = self._function(**self.kwargs)
+        self._unit_converter()
         if self.isfirst:
             self.shape = self.new[0].shape
         return self.isconverged
@@ -730,12 +777,14 @@ class CalcSpinAt(CalcRoutineFunction):
         nc_name = 'spinat'
         nc_dim = ('time', 'natom', 'xyz',)
         nc_unit = 'hbar/2'
+        lammps_unit = 'hbar/2'
         CalcRoutineFunction.__init__(self,
                                      '',
                                      label,
                                      nc_name,
                                      nc_dim,
-                                     nc_unit)
+                                     nc_unit,
+                                     lammps_unit)
 
     def _exec(self, wdir=None):
         """
@@ -748,6 +797,7 @@ class CalcSpinAt(CalcRoutineFunction):
                 self.new = np.r_[[np.zeros((len(_), 3)) for _ in self.atoms]]
         else:
             self.new = self._function(**self.kwargs)
+        self._unit_converter()
         if self.isfirst:
             self.shape = self.new[0].shape
         return self.isconverged
@@ -771,12 +821,14 @@ class CalcElectronicEntropy(CalcRoutineFunction):
         nc_name = 'entropy'
         nc_dim = ('time',)
         nc_unit = ''
+        lammps_unit = ''
         CalcRoutineFunction.__init__(self,
                                      '',
                                      label,
                                      nc_name,
                                      nc_dim,
-                                     nc_unit)
+                                     nc_unit,
+                                     lammps_unit)
 
     def _exec(self, wdir=None):
         """
@@ -790,6 +842,7 @@ class CalcElectronicEntropy(CalcRoutineFunction):
                 self.new = np.r_[[0.0 for _ in self.atoms]]
         else:
             self.new = self._function(**self.kwargs)
+        self._unit_converter()
         if self.isfirst:
             self.shape = self.new[0].shape
         return self.isconverged

mlacs/utilities/io_abinit.py
@@ -128,6 +128,8 @@ class HistFile:
 
         self.ncprefix = ncprefix
         self.launched = launched
+        self._set_name_conventions()
+        self._set_unit_conventions()
 
         if ncpath is not None:
             # Initialize path in a post-processing usage, with given ncpath
@@ -296,7 +298,7 @@ class HistFile:
         """
         with nc.Dataset(self.ncpath, 'r') as ncfile:
             wobs_values = ncfile[obs_name][:]
-            weighted_obs_data = wobs_values[wobs_values.mask == False].data # noqa
+            weighted_obs_data = wobs_values[wobs_values.mask == False].data  # noqa
             weighted_obs_idx = 1 + np.where(~wobs_values.mask)[0]
         return weighted_obs_data, weighted_obs_idx
 
@@ -332,9 +334,9 @@ class HistFile:
         return res
 
 # ========================================================================== #
-    def nc_routine_conv(self):
-        """Define several conventions related to routine properties"""
-        # Variable names and dimensions are those produced by Abinit
+    def _set_name_conventions(self):
+        """Define naming conventions related to routine properties"""
+        # Variable names and dimensions as defined in Abinit
         var_dim_dict = {'Total_Energy': ['etotal', ('time',)],
                         'Kinetic_Energy': ['ekin', ('time',)],
                         'Potential_Energy': ['epot', ('time',)],
@@ -347,22 +349,40 @@ class HistFile:
                         'Stress': ['strten', ('time', 'six')],
                         'Cell': ['rprimd', ('time', 'xyz', 'xyz')]
                         }
+        self.var_dim_dict = var_dim_dict
 
-        # Units correspond to those used in ASE
-        units_dict = {'Total_Energy': 'eV',
-                      'Kinetic_Energy': 'eV',
-                      'Potential_Energy': 'eV',
-                      'Velocities': '',
-                      'Forces': 'eV/Ang',
-                      'Positions': 'Ang',
-                      'Scaled_Positions': 'dimensionless',
-                      'Temperature': 'K',
-                      'Volume': 'Ang^3',
-                      'Stress': 'GPa',
-                      'Cell': 'Ang',
-                      }
+# ========================================================================== #
+    def _set_unit_conventions(self):
+        """Define unit conventions related to routine properties"""
+        # Dict whose keys are 'var names' and values are LAMMPS's 'metal' units
+        lammps_units_dict = {'Total_Energy': 'eV',
+                             'Kinetic_Energy': 'eV',
+                             'Potential_Energy': 'eV',
+                             'Velocities': '',
+                             'Forces': 'eV/Ang',
+                             'Positions': 'Ang',
+                             'Scaled_Positions': 'dimensionless',
+                             'Temperature': 'K',
+                             'Volume': 'Ang^3',
+                             'Stress': 'eV/Ang^3',
+                             'Cell': 'Ang',
+                             }
+        self.lammps_units_dict = lammps_units_dict
 
-        return var_dim_dict, units_dict
+        # Dict whose keys are 'var names' and values are Abinit units
+        abinit_units_dict = {'Total_Energy': 'Ha',
+                             'Kinetic_Energy': 'Ha',
+                             'Potential_Energy': 'Ha',
+                             'Velocities': '',
+                             'Forces': 'Ha/Bohr',
+                             'Positions': 'Bohr',
+                             'Scaled_Positions': 'dimensionless',
+                             'Temperature': 'K',
+                             'Volume': 'Bohr^3',
+                             'Stress': 'Ha/Bohr^3',
+                             'Cell': 'Bohr',
+                             }
+        self.abinit_units_dict = abinit_units_dict
 
 
 # ========================================================================== #
@@ -452,7 +472,7 @@ class AbinitNC:
         which corresponds to the Abinit input file, but also contains unwanted
         (i.e., not encoded in UTF-8) information at the bottom.
         """
-        last_Lammps_line = 'chkexit 1 # abinit.exit file in the running directory' # noqa
+        last_Lammps_line = 'chkexit 1 # abinit.exit file in the running directory'  # noqa
         last_Lammps_line += ' terminates after the current SCF'
         if last_Lammps_line in _str[-len(last_Lammps_line):]:
             return True

mlacs/utilities/plots.py
@@ -13,6 +13,8 @@ from scipy.stats import gaussian_kde
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+from ase.units import Bohr, Hartree, GPa
+
 from . import compute_correlation
 from mlacs.utilities.io_abinit import HistFile
 
@@ -293,7 +295,7 @@ class HistPlot:
         if os.path.isfile(ncpath):
             ncfile = HistFile(ncpath=ncpath)
             dict_var_units = ncfile.get_units()
-            var_dim_dict = ncfile.nc_routine_conv()[0]
+            var_dim_dict = ncfile.var_dim_dict
             dict_name_label = {x[0]: lab for lab, x in var_dim_dict.items()}
             dict_name_label['press'] = 'Pressure'
             self.ncfile = ncfile
@@ -311,6 +313,31 @@ class HistPlot:
             raise FileNotFoundError(msg)
 
 # ========================================================================== #
+    def _custom_unit_converter(self, obs_arr, obs_unit):
+        """
+        Convert units from *HIST.nc atomic units to following custom units:
+            energy: eV,
+            distance: Ang,
+            volume: Ang^3,
+            force: eV/Ang,
+            pressure/stress: GPa
+        """
+        Ha2eV = Hartree
+        Bohr2Ang = Bohr
+        # Format of dict below: {'atomic_unit': [conv_factor, 'custom_unit']}
+        unit_convert_dict = {'Ha': [Ha2eV, 'eV'],
+                             'Bohr': [Bohr2Ang, 'Ang'],
+                             'Ha/Bohr': [Ha2eV/Bohr2Ang, 'eV/Ang'],
+                             'Bohr^3': [Bohr2Ang**3, 'Ang^3'],
+                             'Ha/Bohr^3': [(Ha2eV/Bohr2Ang**3)/GPa, 'GPa'],
+                             }
+        if obs_unit in unit_convert_dict:
+            conv_factor, custom_unit = unit_convert_dict[obs_unit]
+            return obs_arr*conv_factor, custom_unit
+        else:
+            return obs_arr, ''
+
+# ========================================================================== #
     def _core_plot(self, obs_name, fig=None, ax=None):
         """Plot the observable named `obs_name` in the ncfile."""
         if None in (fig, ax):
@@ -321,13 +348,20 @@ class HistPlot:
         dict_var_units = self.dict_var_units
         weights_ncfile = self.weights_ncfile
 
-        observable = ncfile.read_obs(obs_name)
-        obs_label = obs_name
-        if obs_name in dict_name_label:
-            obs_label = dict_name_label[obs_name].replace("_", " ")
+        try:
+            nc_unit = dict_var_units[obs_name]
+        except KeyError:
+            msg = 'No unit found for ' + str(obs_name)
+            nc_unit = ''
+            raise KeyError(msg)
+
+        # Read atomic unit observable from *HIST.nc file
+        obs_au = ncfile.read_obs(obs_name)
+        # Convert to custom visualization units
+        observable, new_unit = self._custom_unit_converter(obs_au, nc_unit)
 
         obs_meta = ncfile.read_obs(obs_name + '_meta')
-        
+
         weights_meta = weights_ncfile.read_obs('weights_meta')
         weights_idx = weights_meta[:, 0]
         nb_effective_conf = weights_meta[:, 1][weights_idx == 1.0]
@@ -342,27 +376,29 @@ class HistPlot:
         # Index of configuration in database
         confs_idx = np.array([i+1 for i in range(len(observable))])
 
-        w_obs_data, w_obs_idx = ncfile.read_weighted_obs('weighted_'+obs_name)
+        w_obs_au, w_obs_idx = ncfile.read_weighted_obs('weighted_'+obs_name)
+        w_obs_data, new_unit = self._custom_unit_converter(w_obs_au, nc_unit)
 
         uniform_obs = np.array([np.mean(observable[:i]) for i in w_obs_idx])
 
         ax.plot(confs_idx, observable, label='raw data', alpha=0.7)
-        ax.plot(w_obs_idx, uniform_obs, c='g', label='uniform weights')
+        ax.plot(w_obs_idx, uniform_obs, c='g', label='uniform weights',
+                marker='.')
 
         if uniform_weight is False:
-            ax.plot(w_obs_idx, w_obs_data, c='r', ls='-', label='mbar')
+            ax.plot(w_obs_idx, w_obs_data, c='r', ls='-', label='mbar',
+                    marker='.')
         xlabel_str = 'Configuration index in database'
         ax.set_xlabel(xlabel_str)
         par_title = (int(len(confs_idx)), int(max(state_idx)),)
         str_title = '# configurations: {}, # states: {}'.format(*par_title)
         fig.suptitle(str_title)
+        obs_label = obs_name
+        if obs_name in dict_name_label:
+            obs_label = dict_name_label[obs_name].replace("_", " ")
         ylabel = obs_label
-        try:
-            obs_unit = dict_var_units[obs_name]
-            ylabel += ' [' + obs_unit + ']'
-        except KeyError:
-            msg = 'No unit found for ' + str(obs_name)
-            raise KeyError(msg)
+        if new_unit != '':
+            ylabel += ' [' + new_unit + ']'
         ax.set_ylabel(ylabel)
 
         legend1 = ax.legend(frameon=False, loc='best')
@@ -413,13 +449,13 @@ class HistPlot:
 
         def _plot_distribution(iter_loc):
             loc_weights_idx = dict_weights[iter_loc][0]
-            # normalized_x = (loc_weights_idx-1)/(loc_weights_idx[-1]-1)
-            normalized_x = (loc_weights_idx-1)
+            normalized_x = loc_weights_idx-1
             loc_weights = dict_weights[iter_loc][1]
             normalized_y = loc_weights/np.mean(loc_weights)
             Nconfs_loc = np.round(nb_effective_conf[iter_loc-1], 1)
             lab_str = r'$N_{\text{eff}} \simeq$'+'{}'.format(Nconfs_loc)
-            ax[1].step(normalized_x, normalized_y, where='mid', label=lab_str, zorder=10-iter_loc)
+            ax[1].step(normalized_x, normalized_y, where='mid',
+                       label=lab_str, zorder=10-iter_loc)
 
         if len(idx_bounds)-1 > 5:
             mlacs_iter_arr = np.geomspace(3, len(idx_bounds)-1, 4, dtype=int)