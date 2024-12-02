f1c3e6cc37980fa174d9c24f8b6880e9aa6a8440
Aloïs Castellano <62248036+OrbitalC@users.noreply.github.com>
2024-12-02 10:39:03 +0100

    Fixing and cleaning the Neti module (#47)
    * Fixing the Neti module
    
    * Fixing the docstring
    
    * Some more correction for NETI and adding tests and examples

--- mlacs/ti/liquids.py
+++ mlacs/ti/liquids.py
@@ -52,10 +52,6 @@ class UFLiquidState(ThermoState):
         to be added to the results.
         If ``None``, no value is added. Default ``None``.
 
-    equilibrate: :class:`Bool` (optional)
-        Equilibrate the ideal strucutre at zero or finite pressure.
-        Default ``True``
-
     p: :class:`int`
         p parameter of the Uhlenbeck-Ford potential.
         Should be ``1``, ``25``, ``50``, ``75`` or ``100``. Default ``50``
@@ -69,6 +65,10 @@ class UFLiquidState(ThermoState):
     damp : :class:`float` (optional)
         Damping parameter.
         If ``None``, a damping parameter of 100 x dt is used.
+
+    pdamp: :class:`float` (optional)
+        Pressure damping parameter, used is the pressure is not `None`
+        By default, this correspond to 1000 times the timestep.
 
     nsteps: :class:`int` (optional)
         Number of production steps. Default ``10000``.
@@ -102,7 +102,6 @@ class UFLiquidState(ThermoState):
 
     loginterval : :class:`int` (optional)
         Number of steps between MLMD logging. Default ``50``.
-
     """
     def __init__(self,
                  atoms,
@@ -380,8 +379,8 @@ class UFLiquidState(ThermoState):
         block2("lambda_ufm", "variable lambda_ufm equal 1-v_lambda_true")
         if len(self.pair_coeff) == 1:
             txt = "pair_style hybrid/scaled " + \
-                  f"v_lambda_true {pair_style[0]} " + \
-                  "v_lambda_ufm ufm ${{rc}}\n"
+                 f"v_lambda_true {self.pair_style} " + \
+                  "v_lambda_ufm ufm ${rc}\n"
             block2("scaling pair_style", txt)
             txt = "pair_coeff " + hybrid_pair_coeff
             block2("true_pair_coeff", txt)
@@ -430,8 +429,8 @@ class UFLiquidState(ThermoState):
         block4("lambda_ufm", "variable lambda_ufm equal 1-v_lambda_true")
         if len(self.pair_coeff) == 1:
             txt = "pair_style hybrid/scaled " + \
-                  f"v_lambda_true {pair_style[0]} " + \
-                  "v_lambda_ufm ufm ${{rc}}\n"
+                  f"v_lambda_true {self.pair_style} " + \
+                  "v_lambda_ufm ufm ${rc}\n"
             block4("scaling pair_style", txt)
             txt = "pair_coeff " + hybrid_pair_coeff
             block4("true_pair_coeff", txt)
